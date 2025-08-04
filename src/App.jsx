a03512f37c9d8a2ba13b4483df71408770c967f3
gthilakshana <gavrawavanniarachchi@gmail.com>
2025-08-04 07:35:15 -0700

    productCard Created 🦀

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+import ProductCard from './components/productCard'
+
+function App() {
+
+
+  return (
+    <>
+      <h1>App</h1>
+      <ProductCard />
+    </>
+  )
+}
+
+export default App
---
+++ src/components/productCard.jsx
@@ -0,0 +1,14 @@
+
+
+export default function ProductCard() {
+
+    return (
+        <div>
+            <h1>ProductCard</h1>
+            <p>Price: 500</p>
+            <img src="" alt="Product" />
+            <button>Add to Cart</button>
+        </div>
+    )
+
+}
